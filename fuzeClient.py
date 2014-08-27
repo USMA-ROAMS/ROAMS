9235b2aec34a810a8f72bda65d2751c06f577dd3
unknown <x55302@USMANBUX55302.usma.ds.army.edu>
2014-08-27 11:33:20 -0400

    Added python files for fuze client and magServer.

---
+++ fuzeClient.py
@@ -0,0 +1,150 @@
+from socket import *
+import threading
+import RPi.GPIO as GPIO
+import time
+global id
+id = '4'
+global setting
+setting = 0
+global gps
+gps = 'AA000000000000'
+global elev
+elev = '00000'
+
+
+class serverCommands(threading.Thread):
+
+   def __init__(self):
+      threading.Thread.__init__(self)
+
+   def run(self):
+      global id, setting, gps, elev
+      host = "192.168.1.1"
+      port = 4445
+      size = 1024
+      ephemeris = ''
+      s = socket(AF_INET, SOCK_STREAM)
+      id = '4'
+      setting = 0
+
+      while True:
+         try:
+            s.connect((host,port))
+            print 'command connection established'
+            break
+         except:
+            pass
+      GPIO.setup(7, GPIO.OUT)
+      GPIO.output(7, True)
+
+      while True:
+         data = s.recv(size)
+         if data:
+            if data[0:3] == '111':
+               ephemeris = data
+               print 'ephemeris received'
+               GPIO.setup(11, GPIO.OUT)
+               GPIO.output(11, True)
+               GPIO.setup(15, GPIO.OUT)
+               #turn on green light
+            else:
+               listSize = (len(data) - 13) / 25
+               print listSize
+               for i in range(0, listSize):
+                  idNum = (i * 25) + 6
+                  if data[idNum] == id:
+                     print id
+                     print data[idNum]
+                     set = data[idNum + 2]
+                     print "Received " + set
+                     GPIO.setup(12, GPIO.OUT)
+                     GPIO.setup(16, GPIO.OUT)
+                     GPIO.setup(18, GPIO.OUT)
+                     GPIO.setup(22, GPIO.OUT)
+                     GPIO.output(12, False)
+                     GPIO.output(16, False)
+                     GPIO.output(18, False)
+                     GPIO.output(22, False)
+                     if set == '0':
+                        setting = 0
+                        print 'impact'
+                        GPIO.output(12, True)
+                     elif set == '1':
+                        setting = 1
+                        print 'delay'
+                        GPIO.output(16, True)
+                     elif set == '2':
+                        setting = 2
+                        print 'near ground'
+                        GPIO.output(18, True)
+                     elif set == '3':
+                        setting = 3
+                        print 'proximity'
+                        GPIO.output(22, True)
+                     else:
+                        print 'error'
+
+                     gpsDataLower = idNum + 4
+                     gpsDataUpper = idNum + 18
+                     gpsData = data[gpsDataLower: gpsDataUpper]
+                     if len(gpsData) == 14:
+                        if gpsData != 'AA000000000000':
+                           gps = gpsData
+
+                     elevDataLower = idNum + 19
+                     elevDataUpper = idNum + 24
+                     elevData = data[elevDataLower: elevDataUpper]
+                     if len(elevData) == 5:
+                        if elevData != '00000':
+                           elev = elevData
+                           GPIO.output(15, True)
+
+class serverPings(threading.Thread):
+
+   def __init__(self):
+      threading.Thread.__init__(self)
+
+
+   def run(self):
+      global id, setting
+      host = "192.168.1.1"
+      port = 4446
+      size = 1024
+      ephemeris = ''
+      s = socket(AF_INET, SOCK_STREAM)
+      id = '4'
+      setting = 0
+
+      while True:
+         try:
+            s.connect((host,port))
+            print 'ping connection established'
+            break
+         except:
+            pass
+      s.send('iam ' + id + ',' + str(setting) + ',' + gps + ',' + elev)
+      print 'info sent'
+
+      while True:
+         data = s.recv(size)
+         if data[0:13] == id + ' acknowledge':
+           s.send(id + ' here,' + str(setting) + ',' + gps + ',' + elev)
+           #print 'acknowledgement sent'
+         elif data[1:13] == ' acknowledge':
+           pass
+         else:
+           print data
+      
+
+class fuzeClient:
+
+   def main(self):
+            commandThread = serverCommands()
+            pingThread = serverPings()
+            commandThread.start()
+            pingThread.start()
+
+
+if __name__=="__main__":
+   client = fuzeClient()
+   client.main()
---
+++ magServerGPS.py
@@ -0,0 +1,278 @@
+import socket
+import threading
+import time
+
+tabHost = '192.168.0.1'
+tabPort = 4444
+piHost = '192.168.1.1'
+piPort = 4445
+pingPort = 4446
+roundThreads = []
+global piServer, tabServer, tabString, pingServer
+piStrings = []
+global piThreads
+global mortars
+global lock
+lock = threading.Lock()
+piThreads = 0
+global ephemeris
+global hasEph
+hasEph = False
+
+class socketThread(threading.Thread):
+
+   def __init__(self, parent, clntSock):
+      global piThreads
+      threading.Thread.__init__(self)
+      self.clntSock = clntSock
+      self.parent = parent
+      if self.parent.serverIP == '192.168.1.1':
+         self.clntNum = piThreads
+         piStrings.append('new round client')
+         piThreads += 1
+
+   def run(self):
+      global tabString, piServer, ephemeris, hasEph
+      while 1:
+         message = self.clntSock.recv(1024)
+         if message:
+            if message == 'close':
+               break
+            if self.parent.serverIP == '192.168.0.1':
+               if message[0:3] == '111':
+                  ephemeris = message
+                  hasEph = True
+                  piServer.sendToAll(ephemeris)
+                  print 'sending ephemeris to rounds'
+               else:
+                  tabString = message #interpret tabString and send to certain rounds
+                  piServer.sendToAll(message)
+                  print 'sending to all rounds: ' + message
+                #Do something with information from tablet
+            if self.parent.serverIP == '192.168.1.1':
+               piStrings[self.clntNum] = message
+               for strings in piStrings:
+                  tabServer.sendToAll("Received from server: " + message)
+               #Do something with information from round
+      self.clntSock.close()
+      print 'socket closed'
+
+
+class updateList(threading.Thread):
+   def __init__(self, parent, clntSock):
+      threading.Thread.__init__(self)
+      self.parent = parent
+      self.clntSock = clntSock
+      self.id = ''
+      self.setting = ''
+      self.gps = ''
+      self.elev = ''
+      
+   def run(self):
+      global mortars, lock
+      self.clntSock.setblocking(0)
+      waitForIAM = True
+      while waitForIAM:
+         #time.sleep(0.5)
+         ack = True
+         try:
+            message = self.clntSock.recv(1024)
+         except:
+            pass
+         try:
+            if message[0:3] == 'iam':
+               fuze = self.update(message[4:])
+               break
+         except:
+            pass
+      self.ping(fuze)
+
+
+
+   def ping(self, fuze):
+     global mortars,lock
+     ack = True
+     while 1: 
+      time.sleep(0.5)
+      self.sendToRound(self.id + ' acknowledge')
+      print 'send ack to: ' + self.id
+      for x in range(0,900000):
+         if x == 899999:
+            ack = False
+         try:
+             message = self.clntSock.recv(1024)
+             if message:
+                print message
+                if message[0:6] == self.id + ' here':
+                   ack = True
+                   if message[7:8] == self.setting:
+                      if message[9:23] == self.gps:
+                         if message[24:] == self.elev:
+                            pass
+                   else:
+                      self.setting = message[7:8]
+                      self.gps = message[9:23]
+                      self.elev = message[24:]
+                      for item in mortars:
+                         if item == fuze:
+                            with lock:
+                               print 'lock acquired'
+                               item[1] = self.setting
+                               item[2] = self.gps
+                               item[3] = self.elev
+                               print 'lock released'
+                            break
+                      print 'data change'
+                      self.sendToTab()
+                   break
+                else:
+                    print 'this round is defunct or fake'
+                    for item in mortars:
+                       if item == fuze:
+                          lock.acquire()
+                          mortars.remove(item)
+                          lock.release()
+                          break
+                    self.sendToTab()
+                    self.clntSock.close()
+                    self.thread.exit()
+                    #self.parent.killThread(self)
+             else:
+                if x == 899999:
+                   print 'outer else'
+                   ack = False
+         except:
+            if x == 899999:
+               print 'pass statement'
+               ack = False
+      if ack == False:
+         print 'remove mortar'
+         for item in mortars:
+            if item == fuze:
+               lock.acquire()
+               mortars.remove(item)
+               lock.release()
+               break
+         self.sendToTab()
+         print 'no ack recieved'
+         self.clntSock.close()
+         return
+
+   def update(self, message):
+      global mortars, lock
+      id = message[0:1]
+      setting = message[2:3]
+      gps = message [4:18]
+      elev = message [19:]
+      fuze = [id,setting,gps,elev]
+      self.id = id
+      self.setting = setting
+      self.gps = gps
+      self.elev = elev
+      mortars.append(fuze)
+      self.sendToTab()
+      return fuze
+
+   def sendToRound(self,message):
+      self.clntSock.send(message)
+
+
+   def sendToTab(self):
+      global mortars
+      string = '<list>'
+      for item in mortars:
+        lock.acquire()
+        string += item[0] + ',' + item[1] + ',' + item[2] + ','+ item[3] + ';'
+        lock.release()
+      string += '</list>\n'
+      print string
+      tabServer.sendToAll(string)
+
+
+
+
+class serverThread(threading.Thread):
+
+   def __init__(self, serverIP, serverPort):
+      threading.Thread.__init__(self)
+      self.threadCount = 0
+      self.myThreads = []
+      self.serverIP = serverIP
+      self.serverPort = serverPort
+      self.sendLock = threading.Lock()
+
+
+   def run(self):
+      global hasEph, ephemeris, pingPort, mortars, piThreads, lock
+      serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+      serverSocket.bind((self.serverIP, self.serverPort))
+      serverSocket.listen(20)
+      while 1:
+         connectionSocket, addr = serverSocket.accept()
+         if self.serverPort == pingPort:
+           pingServer = updateList(self, connectionSocket)
+           self.myThreads.append(pingServer)
+           pingServer.start()
+         else:
+            newClient = socketThread(self, connectionSocket)
+            self.myThreads.append(newClient)
+            newClient.start()
+            if self.serverPort == piPort:
+               lock.acquire()
+               print mortars
+               string = '<list>'
+               for item in mortars:
+                  string += item[0] + ',' + item[1] + ',' + item[2] + ',' + item[3] + ';'
+               string += '</list>\n'
+               try:
+                  print 'Sent to tab: ' + string
+                  tabServer.sendToAll(string)
+               except:
+                 pass
+               lock.release()
+         print 'new client on ' + self.serverIP
+         if hasEph and self.serverIP == '192.168.1.1' and self.serverPort != pingPort:
+            newClient.clntSock.send(ephemeris)
+            print 'ephemeris sent to new client'
+         for thread in self.myThreads:
+            if not thread.isAlive():
+               thread.join()
+               self.myThreads.remove(thread)
+               if self.serverIP == '192.168.1.1':
+                  piThreads -= 1
+
+   def sendToAll(self, message):
+      for thread in self.myThreads:
+         if thread.isAlive():
+            self.sendLock.acquire()
+            thread.clntSock.send(message)
+            self.sendLock.release()
+
+#   def sendToRound(self, message, ip):
+#      for thread in self.myThreads:
+#         if ip == 
+#            self.sendLock.acquire()
+#            thread.clntSock.send(message)
+#            self.sendLock.release()
+
+   def killThread(self,thread):
+      global piThreads
+      thread.join()
+      self.myThreads.remove(thread)
+      if self.serverIP == '192.168.1.1':
+         piThreads -= 1
+      
+
+if __name__ == "__main__":
+   global piServer, tabServer
+   global mortars
+   mortars = []  ## Array of strings representing IDs
+   piServer = serverThread(piHost, piPort)  ## Socket communicating with rounds
+   tabServer = serverThread(tabHost, tabPort)  ## Socket communicating with tablet.
+                                               ## Receives settings from tablet. Calls piServer to send those settings to rounds.
+   pingServer = serverThread(piHost, pingPort)  ## Socket pinging rounds.
+                                                ## Live updates tablet with current rounds and their settings
+   piServer.start()
+   tabServer.start()
+   pingServer.start()
